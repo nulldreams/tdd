ec608a9d9e71f2d7b902b1f5361310ec077dca6b
Igor Martins <igor.martins@tspd-nb-00419.tecnospeed.local>
2019-06-24 15:38:44 -0300

    first commit

---
+++ tests/index.test.js
@@ -0,0 +1,15 @@
+const test = require('tape')
+const index = require('../index')
+const messages = {
+    discount: 'Descontou corretamente'
+}
+
+test('Aplicar desconto', (t) => {
+    t.assert(index.aplicarDesconto(10, 5) === 5, messages.discount)
+    t.end()
+})
+
+test('Aplicar desconto grande', (t) => {
+    t.assert(index.aplicarDesconto(5, 10) === 0, messages.discount)
+    t.end()
+})
